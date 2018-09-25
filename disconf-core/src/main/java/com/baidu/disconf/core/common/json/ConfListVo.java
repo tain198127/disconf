c9bc4e4a584ae17ce2626fcb1016ed675f564590
DaneBrown <tain198127@163.com>
2018-09-25 16:29:51 +0800

    change message type to map<string,object>

--- disconf-core/src/main/java/com/baidu/disconf/core/common/json/ConfListVo.java
+++ disconf-core/src/main/java/com/baidu/disconf/core/common/json/ConfListVo.java
@@ -1,5 +1,7 @@
 package com.baidu.disconf.core.common.json;
 
+import java.util.Map;
+
 /**
  * @author DaneBrown
  * email:tain198127@163.com
@@ -7,14 +9,14 @@ package com.baidu.disconf.core.common.json;
  **/
 public class ConfListVo {
     private boolean success = false;
-    private String message = "";
+    private Map<String,Object> message =null;
     private ConfPageListVo<ClientConfListItemVo> daoPageResult = new ConfPageListVo<ClientConfListItemVo>();
 
     public boolean isSuccess() {
         return success;
     }
 
-    public String getMessage() {
+    public Map<String,Object> getMessage() {
         return message;
     }
 
