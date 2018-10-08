d492366733931631108cdc3821cad283dc13f75f
DaneBrown <tain198127@163.com>
2018-10-08 13:34:16 +0800

    1. remove unnecessary dependency 2. add a new property in disconf.properties for scanpakcages

--- disconf-client/src/main/java/com/baidu/disconf/client/config/DisClientConfig.java
+++ disconf-client/src/main/java/com/baidu/disconf/client/config/DisClientConfig.java
@@ -32,6 +32,10 @@ public final class DisClientConfig {
     // disconf.properties 的路径 -D 传入
     private static final String DISCONF_CONF_FILE_PATH_ARG = "disconf.conf";
 
+    private static final  String DISCONF_CONF_SCAN_PACKAGE="disconf.scanpackages";
+    @DisInnerConfigAnnotation(name = DisClientConfig.DISCONF_CONF_SCAN_PACKAGE)
+    public String ScanPackages;
+
     private boolean isLoaded = false;
 
     private DisClientConfig() {
